363358b4dbf8db4e141080923857dd4e58426c5c
Brittany S <57652157+bsteenbergen@users.noreply.github.com>
2021-09-15 11:49:17 -0700

    Add files via upload
    added files from replit

---
+++ script.js
@@ -0,0 +1,20 @@
+const principalBox = document.querySelector("#principal")
+const intRateBox = document.querySelector("#intRate")
+const compPerYearBox = document.querySelector("#compPerYear")
+const totalYrsBox = document.querySelector("#totalYears")
+const answerBox = document.querySelector("#answer")
+
+function getInterest() {
+  const p = Number(principalBox.value)
+  const r = Number(intRateBox.value)
+  const n = Number(compPerYearBox.value)
+  const t = Number(totalYrsBox.value)
+
+  const interest = p * ((1 + (r / n)) ** (n * t))
+  answerBox.textContent = "The total compound interest is: $" + interest.toFixed(2)
+}
+
+principalBox.addEventListener('input', getInterest)
+intRateBox.addEventListener('input', getInterest)
+compPerYearBox.addEventListener('input', getInterest)
+totalYrsBox.addEventListener('input', getInterest)
