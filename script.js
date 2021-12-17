e79ae3df628111a9916c5661dd968d960fb5ed45
Brittany S <57652157+bsteenbergen@users.noreply.github.com>
2021-12-17 12:41:20 -0800

    fixed names

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const principalBox = document.querySelector("#principal")
 const intRateBox = document.querySelector("#intRate")
 const compPerYearBox = document.querySelector("#compPerYear")
-const totalYrsBox = document.querySelector("#totalYears")
+const totalYearsBox = document.querySelector("#totalYears")
 const answerBox = document.querySelector("#answer")
 
 function getInterest() {
   const p = Number(principalBox.value)
   const r = Number(intRateBox.value)
   const n = Number(compPerYearBox.value)
-  const t = Number(totalYrsBox.value)
+  const t = Number(totalYearsBox.value)
 
   const interest = p * ((1 + (r / n)) ** (n * t))
   answerBox.textContent = "The total compound interest is: $" + interest.toFixed(2)
@@ -17,4 +17,4 @@ function getInterest() {
 principalBox.addEventListener('input', getInterest)
 intRateBox.addEventListener('input', getInterest)
 compPerYearBox.addEventListener('input', getInterest)
-totalYrsBox.addEventListener('input', getInterest)
+totalYearsBox.addEventListener('input', getInterest)
